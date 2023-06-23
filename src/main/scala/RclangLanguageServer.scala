d20bf538fb0788bcdb452f9284a13b6396a8f271
FusionBolt <realfusionbolt@gmail.com>
2023-06-23 17:52:11 +0800

    impl simple hover

--- src/main/scala/RclangLanguageServer.scala
+++ src/main/scala/RclangLanguageServer.scala
@@ -14,7 +14,10 @@ import java.util.concurrent.CompletableFuture
 import concurrent.ExecutionContext.Implicits.global
 import collection.JavaConverters.seqAsJavaListConverter
 
-class RclangLanguageServer extends LanguageServer with WorkspaceService with TextDocumentService with LanguageClientAware { thisServer =>
+// https://javadoc.io/doc/org.eclipse.lsp4j/org.eclipse.lsp4j/latest/index.html
+// https://javadoc.io/static/org.eclipse.lsp4j/org.eclipse.lsp4j/0.21.0/org/eclipse/lsp4j/services/TextDocumentService.html
+class RclangLanguageServer extends LanguageServer with WorkspaceService with TextDocumentService with LanguageClientAware {
+  thisServer =>
   var client: LanguageClient = _
   var rootUri: String = _
 
@@ -54,77 +57,68 @@ class RclangLanguageServer extends LanguageServer with WorkspaceService with Tex
     c.setCompletionProvider(new CompletionOptions(
       /* resolveProvider = */ false,
       /* triggerCharacters = */ List(".").asJava))
+    c.setSignatureHelpProvider(new SignatureHelpOptions(
+      /* triggerCharacters = */ List("(").asJava))
 
     // Do most of the initialization asynchronously so that we can return early
     // from this method and thus let the client know our capabilities.
-//    CompletableFuture.supplyAsync(() => drivers)
+    //    CompletableFuture.supplyAsync(() => drivers)
 
     new InitializeResult(c)
   }
 
-  override def shutdown(): CompletableFuture[AnyRef] = {
-    CompletableFuture[AnyRef]()
+  override def shutdown(): CompletableFuture[Object] = {
+    CompletableFuture.completedFuture(new Object)
   }
 
   override def exit(): Unit = {
-
+    System.exit(0)
   }
 
-  override def getTextDocumentService: TextDocumentService = {
-    this
-  }
+  override def getTextDocumentService: TextDocumentService = this
 
-  override def getWorkspaceService: WorkspaceService = {
-    this
-  }
+  override def getWorkspaceService: WorkspaceService = this
 
   override def connect(client: LanguageClient): Unit = {
     this.client = client
   }
 
-  override def didChangeConfiguration(params: DidChangeConfigurationParams): Unit = {
+  override def didChangeConfiguration(params: DidChangeConfigurationParams): Unit = {}
 
-  }
+  override def didChangeWatchedFiles(params: DidChangeWatchedFilesParams): Unit = {}
 
-  override def didChangeWatchedFiles(params: DidChangeWatchedFilesParams): Unit = {
+  override def didOpen(params: DidOpenTextDocumentParams): Unit = {}
 
-  }
+  override def didChange(params: DidChangeTextDocumentParams): Unit = {}
 
-  override def didOpen(params: DidOpenTextDocumentParams): Unit = {
+  override def didClose(params: DidCloseTextDocumentParams): Unit = {}
 
-  }
+  override def didSave(params: DidSaveTextDocumentParams): Unit = {}
 
-  override def didChange(params: DidChangeTextDocumentParams): Unit = {
-  }
-
-
-  override def didClose(params: DidCloseTextDocumentParams): Unit = {
-
-  }
-
-  override def didSave(params: DidSaveTextDocumentParams): Unit = {
-  }
 
   override def documentSymbol(params: DocumentSymbolParams) = computeAsync { cancelToken =>
-    client.logTrace(new LogTraceParams("document symbol"))
     val uri = params.getTextDocument.getUri
-    client.logMessage(new MessageParams(MessageType.Info, "documentSymbol " + uri))
     val ast = driver(uri)
+    logMessage("documentSymbol " + uri)
+    //    logMessage("ast:" + ast.toString)
+    logMessage("ast:\n" + astToStr(ast))
     val table = symbolTable(ast)
     val symbols = table.classTable.values.flatMap { klass =>
       val methods = klass.methods.values.map { localTable =>
         val method = localTable.astNode
         new DocumentSymbol(method.name.str, SymbolKind.Method,
           method.getPositionRange,
-          method.getPositionRange)}
+          method.getPositionRange)
+      }
       val fields = klass.fields.values.map { field =>
         new DocumentSymbol(field.name.str, SymbolKind.Field,
           field.getPositionRange,
-          field.getPositionRange)}
+          field.getPositionRange)
+      }
       val klassSym = new DocumentSymbol(klass.astNode.name.str, SymbolKind.Class,
         klass.astNode.getPositionRange,
         klass.astNode.getPositionRange)
-      if(klass.astNode.name.str != rclang.Def.Kernel) {
+      if (klass.astNode.name.str != rclang.Def.Kernel) {
         methods.concat(fields).concat(List(klassSym))
       }
       else {
@@ -150,32 +144,50 @@ class RclangLanguageServer extends LanguageServer with WorkspaceService with Tex
   }
 
 
-
   override def documentHighlight(params: DocumentHighlightParams): CompletableFuture[util.List[_ <: DocumentHighlight]] = computeAsync { cancelToken =>
+    // 单击到某个位置会触发
+    logMessage("highlight")
     val uri = params.getTextDocument.getUri
     val ast = driver(uri)
-    val highlights = ast.items.flatMap {
-      case m@Method(decl, body) => List(new DocumentHighlight(
-        m.getPositionRange,
-        DocumentHighlightKind.Text))
-      case _ => Nil
+    val list = getNode(ast, params.getPosition)
+    if(list.isEmpty) {
+      Nil.asJava
+    } else {
+      val x = list.minBy(_.priority)
+      List(new DocumentHighlight(x.getPositionRange, DocumentHighlightKind.Text)).asJava
     }
-    //    highlights.asJava
-    Nil.asJava
   }
 
   override def hover(params: HoverParams): CompletableFuture[Hover] = computeAsync { cancelToken =>
     val uri = params.getTextDocument.getUri
     val ast = driver(uri)
-    val hover = ast.items.flatMap {
-      case m@Method(decl, body) => List(new Hover(
-        new MarkupContent("plaintext", m.toString),
-        m.getPositionRange))
-      case _ => Nil
+
+    logMessage(f"hover: ${params.getPosition}");
+    // get Position
+    // get ast node on position
+    // show content about ast node
+    val list = getNode(ast, params.getPosition)
+    logMessage(list.map(_.toString).mkString("\n\n"))
+    if list.isEmpty then {
+      new Hover(new MarkupContent("plaintext", "not found"))
     }
-    hover.head
+    else {
+      // sort and get the minimal one
+      val x = list.maxBy(_.priority)
+      val str = x match
+        case klass: rclang.ast.Class => s"class ${klass.name.str}"
+        case method: rclang.ast.Method => s"method ${method.name.str}"
+        case _ => s"${x.getClass.getSimpleName}: ${x.toString}"
+      val content = new MarkupContent("plaintext", str)
+      new Hover(content)
+    }
   }
 
+  private def logMessage(message: String): Unit = {
+    client.logMessage(new MessageParams(MessageType.Info, message));
+  }
+
+  // go to definition的时候reference也会一起触发
   override def definition(params: DefinitionParams): CompletableFuture[JEither[util.List[_ <: Location], util.List[_ <: LocationLink]]] = computeAsync { cancelToken =>
     val uri = params.getTextDocument.getUri
     val ast = driver(uri)
@@ -183,12 +195,8 @@ class RclangLanguageServer extends LanguageServer with WorkspaceService with Tex
       case m@Method(decl, body) => List(new Location(uri, m.getPositionRange))
       case _ => Nil
     }
+    logMessage("definition")
     JEither.forLeft(locations.asJava)
-  }
-
-  override def rename(params: RenameParams): CompletableFuture[WorkspaceEdit] = computeAsync { cancelToken =>
-    val uri = params.getTextDocument.getUri
-    new WorkspaceEdit()
   }
 
   override def references(params: ReferenceParams): CompletableFuture[util.List[_ <: Location]] = computeAsync { cancelToken =>
@@ -198,6 +206,77 @@ class RclangLanguageServer extends LanguageServer with WorkspaceService with Tex
       case m@Method(decl, body) => List(new Location(uri, m.getPositionRange))
       case _ => Nil
     }
+    logMessage("reference")
     locations.asJava
   }
+
+  // 输入完新的名字以后会触发
+  override def rename(params: RenameParams): CompletableFuture[WorkspaceEdit] = computeAsync { cancelToken =>
+    val uri = params.getTextDocument.getUri
+    logMessage("rename")
+    new WorkspaceEdit()
+  }
+
+  override def signatureHelp(params: SignatureHelpParams): CompletableFuture[SignatureHelp] = computeAsync { cancelToken =>
+    val uri = params.getTextDocument.getUri
+    val ast = driver(uri)
+    val signatureHelp = ast.items.flatMap {
+      case m@Method(decl, body) => List(new SignatureInformation(m.toString))
+      case _ => Nil
+    }
+    logMessage("signatureHelp")
+    new SignatureHelp(signatureHelp.asJava, 1, 1)
+  }
+
+  override def symbol(params: WorkspaceSymbolParams): CompletableFuture[JEither[util.List[_ <: SymbolInformation], util.List[_ <: WorkspaceSymbol]]] = computeAsync { cancelToken =>
+//    val uri = params.getQuery
+//    val ast = driver(uri)
+//    val symbols = ast.items.flatMap {
+//      case m@Method(decl, body) => List(new SymbolInformation(m.toString, SymbolKind.Method, m.getPositionRange))
+//      case _ => Nil
+//    }
+    JEither.forLeft(Nil.asJava)
+  }
+
+  override def executeCommand(params: ExecuteCommandParams): CompletableFuture[AnyRef] = computeAsync { cancelToken =>
+    logMessage("executeCommand")
+    null
+  }
+
+  // fix or refactor, e.g. alt + enter
+  override def codeAction(params: CodeActionParams): CompletableFuture[util.List[JEither[Command, CodeAction]]] = super.codeAction(params)
+
+  // A code lens represents a command that should be shown along with source text, like the number of references, a way to run tests, etc.
+  // 是不是类似于在上面显示一个[Run]一样的东西
+  override def codeLens(params: CodeLensParams): CompletableFuture[util.List[_ <: CodeLens]] = super.codeLens(params)
+
+  // A document link is a range in a text document that links to an internal or external resource, like another text document or a web site.
+  override def documentLink(params: DocumentLinkParams): CompletableFuture[util.List[DocumentLink]] = super.documentLink(params)
+
+
+  override def documentColor(params: DocumentColorParams): CompletableFuture[util.List[ColorInformation]] = super.documentColor(params)
+
+  override def colorPresentation(params: ColorPresentationParams): CompletableFuture[util.List[ColorPresentation]] = super.colorPresentation(params)
+
+  override def foldingRange(params: FoldingRangeRequestParams): CompletableFuture[util.List[FoldingRange]] = super.foldingRange(params)
+
+  override def prepareCallHierarchy(params: CallHierarchyPrepareParams): CompletableFuture[util.List[CallHierarchyItem]] = super.prepareCallHierarchy(params)
+
+  override def callHierarchyIncomingCalls(params: CallHierarchyIncomingCallsParams): CompletableFuture[util.List[CallHierarchyIncomingCall]] = super.callHierarchyIncomingCalls(params)
+
+  override def callHierarchyOutgoingCalls(params: CallHierarchyOutgoingCallsParams): CompletableFuture[util.List[CallHierarchyOutgoingCall]] = super.callHierarchyOutgoingCalls(params)
+
+  override def semanticTokensFull(params: SemanticTokensParams): CompletableFuture[SemanticTokens] = super.semanticTokensFull(params)
+
+  override def semanticTokensFullDelta(params: SemanticTokensDeltaParams): CompletableFuture[JEither[SemanticTokens, SemanticTokensDelta]] = super.semanticTokensFullDelta(params)
+
+  override def semanticTokensRange(params: SemanticTokensRangeParams): CompletableFuture[SemanticTokens] = super.semanticTokensRange(params)
+
+  override def moniker(params: MonikerParams): CompletableFuture[util.List[Moniker]] = super.moniker(params)
+
+  override def inlayHint(params: InlayHintParams): CompletableFuture[util.List[InlayHint]] = super.inlayHint(params)
+
+  override def inlineValue(params: InlineValueParams): CompletableFuture[util.List[InlineValue]] = super.inlineValue(params)
+
+  override def diagnostic(params: DocumentDiagnosticParams): CompletableFuture[DocumentDiagnosticReport] = super.diagnostic(params)
 }
--- src/main/scala/RclangPart.scala
+++ src/main/scala/RclangPart.scala
@@ -6,6 +6,7 @@ import java.io.File
 import java.net.URI
 import rclang.tools.{DumpManager, GlobalTable, unwrap}
 import org.eclipse.lsp4j.*
+import rclang.ast.Expr.Self
 
 import scala.collection.immutable
 
@@ -34,14 +35,186 @@ def initRclang() = {
 
 def keywords = rclang.lexer.Keyword.values
 
-extension (p: ASTNode) {
-  def getPosition = new Position(p.pos.line, p.pos.column)
+// position to ast node
+def getNode(ast: RcModule, position: Position) = {
+  val searcher = new NodeSearcher(position)
+  searcher.searchModule(ast)
+}
 
-  def getPositionRange = p match
+class ASTPrinter extends rclang.ast.ASTVisitor {
+  var result = ""
+  val indent = "\t"
+  var level = 0;
+  var end = true
+
+  def IndentStr = {
+    if end then indent * level else ""
+  }
+  def write(s: String): Unit = {
+    result += IndentStr + s
+    end = false
+  }
+
+  def writeLine(s: String, node: ASTNode) = {
+    result += IndentStr + s + s"<${node.getPositionRange.simplyStr}>"+ "\n"
+    end = true
+  }
+
+  def doIndent(f: () => Unit): Unit = {
+    level += 1
+    f()
+    level -= 1
+  }
+
+  override def visit(modules: Modules): R = {
+    modules.modules.map(visit).mkString("\n")
+  }
+
+  override def visit(module: RcModule): R = {
+    writeLine(s"RcModule ${module.name}", module)
+    doIndent(() => {
+      module.items.map(visit)
+    })
+  }
+
+  override def visit(method: Method): R = {
+    writeLine(s"Method ${method.decl.name.str}", method)
+    doIndent(() => {
+      visit(method.decl)
+      visit(method.body)
+    })
+  }
+
+  override def visit(klass: Class): R = {
+    writeLine(s"Class ${klass.name}", klass)
+    doIndent(() => {
+      klass.parent match
+        case Some(parent) => write("Parent:");visit(parent)
+        case None =>
+      klass.vars.map(visit)
+      klass.methods.map(visit)
+      klass.generic match
+        case Some(generic) => visit(generic)
+        case None =>
+    })
+  }
+
+  override def visit(expr: Expr): R = {
+    writeLine("expr", expr)
+  }
+
+  override def visit(stmt: Stmt): R = {
+    writeLine(stmt.getClass.getSimpleName, stmt)
+  }
+
+  override def visit(ty: TyInfo): R = writeLine(s"TyInfo ${ty.getClass.getSimpleName}", ty)
+
+  override def visit(decl: MethodDecl): R = {
+    writeLine(s"MethodDecl ${decl.name.str}", decl)
+    doIndent(() => {
+      decl.inputs.params.map(visit)
+      visit(decl.outType)
+    })
+  }
+
+  override def visit(ident: Ident): R = writeLine(s"Ident ${ident.str}", ident)
+
+  override def visit(param: Param): R = writeLine(s"Param ${param.name.str}", param)
+
+  override def visit(field: FieldDef): R = writeLine(s"FieldDef ${field.name.str}", field)
+}
+
+
+class NodeSearcher(val position: Position) {
+  def searchModule(ast: RcModule): List[ASTNode] = {
+    ast.items.flatMap(searchItem)
+  }
+
+  def searchItem(ast: Item): List[ASTNode] = {
+    ast match
+      case m@Method(decl, body) => searchMethod(m)
+      case c@Class(name, parent, vars, methods, generic) => searchClass(c)
+      case _ => List()
+  }
+
+  def searchMethod(ast: Method): List[ASTNode] = {
+    ast.decl.containsPosition(position) match
+      case true => List(ast.decl, ast)
+      case false => {
+        val body = searchExpr(ast.body)
+        if body.isEmpty then body else body.appended(ast)
+      }
+  }
+
+  def searchClass(klass: rclang.ast.Class): List[ASTNode] = {
+    var list = List[ASTNode](klass)
+    list = klass.parent match
+      case Some(parent) => list :+ parent
+      case None => list
+    list = list.concat(klass.vars).concat(klass.methods)
+    list = klass.generic match
+      case Some(generic) => list :+ generic
+      case None => list
+    val resultList = list.filter(ast => {
+      ast.containsPosition(position)
+    })
+    if resultList.isEmpty then resultList else resultList
+  }
+
+  def searchExpr(expr: Expr): List[ASTNode] = {
+    if(!expr.containsPosition(position)) {
+      List()
+    } else {
+      List(expr)
+//      val list = expr.operands.map{op => {
+//        op match
+//          case expr@Expr => searchExpr(expr.asInstanceOf[Expr])
+//          case _ => Some(op)
+//      }}
+//      list
+    }
+  }
+}
+
+def astToStr(ast: RcModule) = {
+  val p = new ASTPrinter()
+  p.visit(ast)
+  p.result
+}
+
+extension (r: Range) {
+  def contains(position: Position): Boolean = {
+    val line = position.getLine
+    val col = position.getCharacter
+    val start = r.getStart
+    val end = r.getEnd
+    start.getLine < line && line < end.getLine ||
+      (start.getLine == line && start.getCharacter <= col) ||
+      (end.getLine == line && end.getCharacter >= col)
+  }
+
+  def simplyStr: String = {
+    s"${r.getStart.getLine}:${r.getStart.getCharacter}, ${r.getEnd.getLine}:${r.getEnd.getCharacter}"
+  }
+}
+
+extension (p: ASTNode) {
+  def getPosition = new Position(p.pos.line - 1, p.pos.column - 1)
+
+  def getPositionRange: Range = p match
     case method: Method => PositionRange.getMethodPositionRange(method)
     case fieldDef: FieldDef => PositionRange.getFieldPositionRange(fieldDef)
     case klass: rclang.ast.Class => PositionRange.getClassPositionRange(klass)
     case _ => new Range(p.getPosition, p.getPosition)
+
+  def containsPosition(position: Position) = p.getPositionRange.contains(position)
+
+  def priority = {
+    p match
+      case _: rclang.ast.Class => 1
+      case _: Method => 2
+      case _ => 0
+  }
 }
 
 object PositionRange {
